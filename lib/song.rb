8a9378c452afed1f4a5eadc3059dd800e25b53bc
corym <cmarker4@gmail.com>
2020-11-11 17:00:09 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,42 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+ 
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |i|
+      if i == @name
+        song.name
+        return
+      end
+    end
   end
 
 end
