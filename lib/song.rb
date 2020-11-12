cb5442c6a461ab37a31d6cd517682571f4a8eef5
cmarker <cmarker4@gmail.com>
2020-11-12 14:31:51 -0600

    commit tree fdb653849eca492c2cca02f26e2ea9879f9afb84

--- lib/song.rb
+++ lib/song.rb
@@ -32,12 +32,38 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.each do |i|
-      if i == @name
-        song.name
-        return
-      end
+    self.all.find {|i| i.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
     end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|i| i.name}
+  end
+
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
